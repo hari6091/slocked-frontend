29d1148096fb1a69a1d6c0fee0c196e1c915043b
hari6091 <hari.junior19@gmail.com>
2023-10-23 17:26:49 -0300

    feat: add backend endpoint for docker compose

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://backend:5000',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
