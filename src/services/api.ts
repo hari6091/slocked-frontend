dadc9e178d34df998bea59c6dfdc6875534f9b08
root <root@localhost>
2023-10-23 17:54:11 -0300

    fix: change api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
