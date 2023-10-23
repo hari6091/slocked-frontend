8c68de7c6c94bf6d43eca0304ff611fcc1be9ec0
hari6091 <hari.junior19@gmail.com>
2023-10-23 17:34:40 -0300

    feat: update baseURL to connect with backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://backend:5000',
+  baseURL: 'http://172.24.0.3:5000',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
